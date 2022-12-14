83ee9358e000e188c0eca21a8efacf897c84a7a0
ArthurPlohoy <arturplokhyi@gmail.com>
2022-12-14 16:01:00 +0200

    Navigations

---
+++ cypress/e2e/webdriveruniversity/browser-navigation.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe("Validate homepage links", () => {
+    it("Confirm links redirect to the correct pages", () => {
+        //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.visit("http://www.webdriveruniversity.com/");
+        cy.get('#contact-us').invoke('removeAttr', 'target').click({force:true});
+        cy.url().should('include', 'contactus')
+
+        cy.go('back');
+        cy.reload();
+        //cy.reload(true); //reload without using cache 
+
+    });
+})
